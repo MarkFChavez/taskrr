b8ed7468dca7300d92ae45a90dc96baf59514cf8
= <los_populares@yahoo.com>
2013-05-04 08:54:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskrr::Application.config.secret_token = '850f48d3fceee592bd7401878cda144f7b4a065f59500b9aec1d8c451cf1c323b057afc462ec079186b097d577a22e5bf6762e56c8a5713896b6fdbb2b6fcd5a'
