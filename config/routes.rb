34591c93176b582d3a47a25af212c7376c75f525
= <los_populares@yahoo.com>
2013-05-04 09:37:38 -0700

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Taskrr::Application.routes.draw do
   
-  root :to => "home/index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
