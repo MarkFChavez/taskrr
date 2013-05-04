b601213471a5cbb293371a3caa7c3e804cd5c26b
= <los_populares@yahoo.com>
2013-05-04 09:30:50 -0700

    Added twitter gem

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "YtyzIvMvrGxSxyyf1AuzdA"
+  config.consumer_secret = "ENfo15DPQiVzKGp1SrtMaTg9wIIAIzDZlor1bZw"
+  config.oauth_token = "1402596174-JAvVJqzLUfSuI7OVD2SoJg90yPH0hAc0l484vkC"
+  config.oauth_token_secret = "uASbSTMjDHWBqNN2idRAtLkl1ssarq6HcvVkEoQSk"
+end
